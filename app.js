88d250c1c71f0d17a4ef30e0781b7b344209fdc4
Cesar De La Vega <you@example.com>
2019-10-25 12:15:20 -0400

    Jquery API call works

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 
+//request with Jquery
+$("#jquery").click(function(){ 
+  $.getJSON(url)
+    .done(function(data){
+      $( "#quote" ).html( data );
+      })
+    .fail(function(){
+      console.log("error");
+    })
+  });
 
+//request with Axios
+
+//request with XHR
+
+//request with FETC
