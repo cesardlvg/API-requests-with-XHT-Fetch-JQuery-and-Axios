2c2f41d1dbeea9fcb302abdf84fa268a29150003
Cesar De La Vega <you@example.com>
2019-10-25 13:20:54 -0400

    added fetch and XHR methods

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
-var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes'
+var xhrbtn = document.querySelector("#xhr");
+var fetchbtn = document.querySelector("#fetch");
+var axiosbtn = document.querySelector("#axios");
+var display = document.querySelector("#quote");
 
-//request with Jquery
-$("#jquery").click(function(){ 
-  $.getJSON(url)
-    .done(function(data){
-      $( "#quote" ).html( data );
-      })
-    .fail(function(){
-      console.log("error");
-    })
-  });
-
-//request with Axios
-$("#axios").click(function(){
-axios.get(url)
-.then(function(res){
-    $( "#quote" ).html(res.data);
-})
-.catch(function(){
-    console.log("error");
-})
+xhrbtn.addEventListener("click", function(){
+  var XHR = new XMLHttpRequest();
+  XHR.onreadystatechange = function(){
+    if(XHR.readyState == 4 && XHR.status == 200){
+      var quote = JSON.parse(XHR.responseText)[0];
+      display.innerText = quote;
+    }
+  }
+  XHR.open("GET", url);
+  XHR.send();
 });
 
-//request with XHR
 
-//request with FETC
+fetchbtn.addEventListener("click", function(){
+  fetch(url)
+  .then(function(req){
+    req.json().then(function(data){
+      display.innerText = data[0];
+    })
+  })
+  .catch(function(){
+    alert("ERROR!")
+  })
+});
+
+
+
+$('#jquery').click(function(){
+  $.getJSON(url)
+  .done(function(data){
+    $('#quote').text(data[0]);
+  });
+});
+
+
+axiosbtn.addEventListener("click",function(){
+  axios.get(url)
+  .then(function(res){
+    display.innerText = res.data[0];
+  })
+  .catch(function(){
+    alert("ERROR!");
+  })
+});
